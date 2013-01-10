cc249d7d14c75156e9139f55d473737706d1586c
Felix Klinge <f.klinge@k15games.de>
2013-01-11 00:57:16 +0100

    Initial commit

---
+++ HühnerRennen/Chicken.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Controls;
+using System.IO;
+
+namespace HühnerRennen
+{
+    class Chicken
+    {
+        private static Random r = new Random();
+        private int m_posX;
+        private int m_posY;
+        private double m_curAnimationTime;
+
+        private static double m_animationTime = 7.0;
+
+        private Image[] m_animation;
+        private int m_curAnimation;
+        private double m_speed;
+
+        public enum ChickenType
+        {
+            GREEN_CHICKEN = 0,
+            RED_CHICKEN,
+            BLUE_CHICKEN,
+            VIOLET_CHICKEN
+        }
+
+        public Image Graphic
+        {
+            get { return m_animation[m_curAnimation]; }
+        }
+
+        public double Speed
+        {
+            get { return m_speed; }
+            set { m_speed = value; }
+        }
+
+        public int X
+        {
+            get { return m_posX; }
+            set { m_posX = value; }
+        }
+        public int Y
+        {
+            get { return m_posY; }
+            set { m_posY = value; }
+        }
+
+        public Chicken(ChickenType type)
+        {
+            m_speed = 0;
+            m_curAnimation = 0;
+            m_curAnimationTime = m_animationTime;
+            m_animation = new System.Windows.Controls.Image[2];
+
+            m_animation[0] = new Image();
+            m_animation[1] = new Image();
+
+            _LoadAnimation(type);
+        }
+
+        public void CalculateSpeed()
+        {
+            m_speed = r.Next(10);
+
+            m_curAnimationTime -= m_speed;
+
+            //Wechsle die Animation.
+            if (m_curAnimationTime < 0.0)
+            {
+                m_curAnimationTime = m_animationTime;
+                m_curAnimation = m_curAnimation == 0 ? 1 : 0;
+            }
+
+            m_posX += (int)(m_speed * r.NextDouble());
+        }
+
+        private void _LoadAnimation(ChickenType type)
+        {
+            if (type == ChickenType.BLUE_CHICKEN)
+            {
+                m_animation[0].Source = ChickenResources.BlueChicken_1;
+                m_animation[1].Source = ChickenResources.BlueChicken_2;
+            }
+            else if (type == ChickenType.GREEN_CHICKEN)
+            {
+                m_animation[0].Source = ChickenResources.GreenChicken_1;
+                m_animation[1].Source = ChickenResources.GreenChicken_2;
+            }
+            else if (type == ChickenType.RED_CHICKEN)
+            {
+                m_animation[0].Source = ChickenResources.RedChicken_1;
+                m_animation[1].Source = ChickenResources.RedChicken_2;
+            }
+            else
+            {
+                m_animation[0].Source = ChickenResources.VioletChicken_1;
+                m_animation[1].Source = ChickenResources.VioletChicken_2;
+            }
+        }
+    }
+}
---
+++ HühnerRennen/ChickenResources.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media.Imaging;
+
+namespace HühnerRennen
+{
+    static class ChickenResources
+    {
+        public static BitmapImage GreenChicken_1;
+        public static BitmapImage GreenChicken_2;
+
+        public static BitmapImage RedChicken_1;
+        public static BitmapImage RedChicken_2;
+
+        public static BitmapImage BlueChicken_1;
+        public static BitmapImage BlueChicken_2;
+
+        public static BitmapImage VioletChicken_1;
+        public static BitmapImage VioletChicken_2;
+
+        public static void LoadResources()
+        {
+            int x = 0;
+            int y = 0;
+
+            const int width = 40;
+            const int height = 34;
+
+            Bitmap original = new Bitmap("ducks.png");
+            
+
+            for (int i = 0; i < 4; ++i)
+            {
+                MemoryStream stream1 = new MemoryStream();
+                MemoryStream stream2 = new MemoryStream();
+
+                Bitmap animation_1 = original.Clone(new Rectangle(x, y, width, height), original.PixelFormat);
+
+                y += height;
+
+                Bitmap animation_2 = original.Clone(new Rectangle(x, y, width, height), original.PixelFormat);
+
+                x += width;
+                y = 0;
+
+                animation_1.Save(stream1, System.Drawing.Imaging.ImageFormat.Png);
+                animation_2.Save(stream2, System.Drawing.Imaging.ImageFormat.Png);
+
+                if (i == 0)
+                {
+                    GreenChicken_1 = new BitmapImage();
+                    GreenChicken_2 = new BitmapImage();
+
+                    GreenChicken_1.BeginInit();
+                    GreenChicken_1.StreamSource = stream1;
+                    GreenChicken_1.EndInit();
+
+                    GreenChicken_2.BeginInit();
+                    GreenChicken_2.StreamSource = stream2;
+                    GreenChicken_2.EndInit();
+                }
+                else if (i == 1)
+                {
+                    RedChicken_1 = new BitmapImage();
+                    RedChicken_2 = new BitmapImage();
+
+                    RedChicken_1.BeginInit();
+                    RedChicken_1.StreamSource = stream1;
+                    RedChicken_1.EndInit();
+
+                    RedChicken_2.BeginInit();
+                    RedChicken_2.StreamSource = stream2;
+                    RedChicken_2.EndInit();
+                }
+                else if (i == 2)
+                {
+                    BlueChicken_1 = new BitmapImage();
+                    BlueChicken_2 = new BitmapImage();
+
+                    BlueChicken_1.BeginInit();
+                    BlueChicken_1.StreamSource = stream1;
+                    BlueChicken_1.EndInit();
+
+                    BlueChicken_2.BeginInit();
+                    BlueChicken_2.StreamSource = stream2;
+                    BlueChicken_2.EndInit();
+                }
+                else
+                {
+                    VioletChicken_1 = new BitmapImage();
+                    VioletChicken_2 = new BitmapImage();
+
+                    VioletChicken_1.BeginInit();
+                    VioletChicken_1.StreamSource = stream1;
+                    VioletChicken_1.EndInit();
+
+                    VioletChicken_2.BeginInit();
+                    VioletChicken_2.StreamSource = stream2;
+                    VioletChicken_2.EndInit();
+                }
+            }
+        }
+    }
+}
